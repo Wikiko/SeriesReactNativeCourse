0345b0eab882845ca7c5cd59b7f2efb79208c8a7
William Azevedo <william.azevedo@papersolutions.com.br>
2018-07-23 17:06:05 -0300

    Criando FormRow

--- App.js
+++ App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import LoginPage from './src/pages/LoginPage';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+export default createStackNavigator(
+  {
+    Login: LoginPage
   },
-});
+  {
+    navigationOptions: {
+      title: 'Series',
+      headerTintColor: 'white',
+      headerTitleStyle: {
+        fontSize: 30,
+        color: 'white'
+      },
+      headerStyle: {
+        backgroundColor: '#6ca2f7'
+      }
+    }
+  }
+);
