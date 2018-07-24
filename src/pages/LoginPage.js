0acd8a1321c95ddf767e35610def29fac270501a
William Raimundo Azevedo <willianray@gmail.com>
2018-07-23 23:19:08 -0300

    Login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Button, TextInput, StyleSheet } from 'react-native';
 import FormRow from '../components/FormRow';
 
 export default class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mail: '',
+            password: ''
+        };
+
+        this.onChangeHandler = this.onChangeHandler.bind(this);
+        this.tryLogin = this.tryLogin.bind(this);
+    }
+
     static navigationOptions = {
         title: 'Bem vindo'
     };
+
+    tryLogin(){
+        console.log(this.state);
+    }
+
+    onChangeHandler(field) {
+        return (value) => {
+            this.setState({
+                [field]: value
+            });
+        }
+    }
 
     render() {
         return (
@@ -14,6 +37,8 @@ export default class LoginPage extends React.Component {
                     <TextInput
                         style={styles.input}
                         placeholder="user@mail.com"
+                        value={this.state.mail}
+                        onChangeText={this.onChangeHandler('mail')}
                     />
                 </FormRow>
                 <FormRow>
@@ -21,6 +46,14 @@ export default class LoginPage extends React.Component {
                         style={styles.input}
                         placeholder="******"
                         secureTextEntry
+                        value={this.state.password}
+                        onChangeText={this.onChangeHandler('password')}
+                    />
+                </FormRow>
+                <FormRow>
+                    <Button
+                        title='Entrar'
+                        onPress={this.tryLogin}
                     />
                 </FormRow>
             </View>
